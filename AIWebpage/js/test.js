98337e78616daaf3aed6005372c1b0b726430485
krishnajha23 <krishjhaxd@gmail.com>
2024-01-18 12:57:58 -0500

    Update test.js

--- AIWebpage/js/test.js
+++ AIWebpage/js/test.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 var HTMLParser = require('node-html-parser');
 async function scrapeSite() {
-	const url = `https://en.wikipedia.org/wiki/Main_Page`;
+	const url = `https://krishjhaxd.wordpress.com/2023/12/29/beyond-the-obstacle/`;
 	const { data } = await axios.get(url);
 	return data
 }
 
 scrapeSite().then(result => {
-    var htmldoc = HTMLParser.parse(result);
-    ptag1 = htmldoc.getElementsByTagName("p")[2]
-    console.log(ptag1.toString())
+    var htmldoc = HTMLParser.parse(result)
+    post1 = htmldoc.getElementsByTagName("p")[2].innerText
+    console.log(post1)
 	}).catch(err => console.log(err));
