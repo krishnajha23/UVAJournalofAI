98ec214f2dc7320de18cd2aff9d6ebacba237384
winstonaviation <winstonshek2005@gmail.com>
2024-01-18 16:24:23 -0500

    Update test.js

--- AIWebpage/js/test.js
+++ AIWebpage/js/test.js
@@ -1,13 +1,23 @@
 const axios = require('axios');
 var HTMLParser = require('node-html-parser');
 async function scrapeSite() {
-	const url = `https://krishjhaxd.wordpress.com/2023/12/29/beyond-the-obstacle/`;
+	const url = `https://krishjhaxd.wordpress.com/2024/01/09/test/`;
 	const { data } = await axios.get(url);
 	return data
 }
 
 scrapeSite().then(result => {
-    var htmldoc = HTMLParser.parse(result)
-    post1 = htmldoc.getElementsByTagName("p")[2].innerText
-    console.log(post1)
+    var htmldoc = HTMLParser.parse(result);
+	title = htmldoc.getElementsByTagName("h2")[0].innerText;
+	console.log(title);
+	for(let i=0; i<htmldoc.getElementsByTagName("p").length; i++){
+		console.log(htmldoc.getElementsByTagName("p")[i].innerText);
+		console.log(htmldoc.getElementsByTagName("p")[i].classNames);
+	}
+    descripOfArticle = htmldoc.getElementsByTagName("p")[1].innerText;
+	
+	post3 = htmldoc.getElementsByTagName("p")[2].innerText;
+	post4 = htmldoc.getElementsByTagName("p")[3].innerText;
+	console.log(descripOfArticle);
+    console.log(post3);
 	}).catch(err => console.log(err));
